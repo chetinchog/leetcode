e96844458561dec8ac8c3e51a33c427e20d71c96
Martin <chetinchog@yahoo.com>
2020-05-07 01:04:57 -0300

    NumJewelsInStones info

--- easy/numJewelsInStones.go
+++ easy/numJewelsInStones.go
@@ -1,6 +1,11 @@
 package easy
 
-//NumJewelsInStones (J string, S string) int
+/*
+ * 771. Jewels and Stones
+ * https://leetcode.com/problems/jewels-and-stones/
+ */
+
+//NumJewelsInStones - Runetime: 0 ms - Memory: 2 MB
 func NumJewelsInStones(J string, S string) int {
 	n := 0
 	for _, t := range J {
