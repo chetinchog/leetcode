9afc6456e13963248ebfc98531e65a68896dd842
Martin <chetinchog@yahoo.com>
2020-05-07 01:04:46 -0300

    KidsWithCandies info

--- easy/kidsWithCandies.go
+++ easy/kidsWithCandies.go
@@ -1,5 +1,10 @@
 package easy
 
+/*
+ * 1431. Kids With the Greatest Number of Candies
+ * https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+ */
+
 //KidsWithCandies - Runetime: 0 ms - Memory: 2.3 MB
 func KidsWithCandies(candies []int, extraCandies int) []bool {
 	r := []bool{}
