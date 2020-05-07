8a3bc00f393c9c4069188f60cc89467bfeb95b0b
Martin <chetinchog@yahoo.com>
2020-05-07 01:05:04 -0300

    TwoSum info

--- easy/twoSum.go
+++ easy/twoSum.go
@@ -1,6 +1,11 @@
 package easy
 
-//TwoSum (nums []int, target int)
+/*
+ * 1. Two Sum
+ * https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+ */
+
+//TwoSum - Runetime: 20 ms - Memory: 2.9 MB
 func TwoSum(nums []int, target int) []int {
 	for i, v := range nums {
 		x := target - v
